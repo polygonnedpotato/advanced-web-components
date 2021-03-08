20c30ad571c018dfe4789d093ce1fd83338aebe2
PolygonnedPotato <cuteiethecorgi@gmail.com>
2021-03-08 09:23:49 -0800

    more new stuff

---
+++ internal/runtime/background.js
@@ -0,0 +1,13 @@
+chrome.runtime.onInstalled.addListener(function () {
+    const kAWCActions = {
+        'wmanextpage': '[AWC WMan] Open in an external window',
+    };
+    for (let key of Object.keys(kAWCActions)) {
+        chrome.contextMenus.create({
+            id: key,
+            title: kAWCActions[key],
+            type: 'normal',
+            contexts: ['selection'],
+        });
+    }
+});
