99651809bd342f89ff6e7793c95c6432d4029a49
PolygonnedPotato <cuteiethecorgi@gmail.com>
2021-03-08 11:21:21 -0800

    morestuff

--- background.js
+++ background.js
@@ -1,5 +1,17 @@
 import * as startup from '/internal/runtime/startup.js';
 
-chrome.runtime.onInstalled(function() {
+chrome.runtime.onInstalled.addListener(function () {
+    const kAWCActions = {
+        'wmanextpage': '[AWC WMan] Open in an external window',
+    };
+    for (let key of Object.keys(kAWCActions)) {
+        chrome.contextMenus.create({
+            id: key,
+            title: kAWCActions[key],
+            type: 'normal',
+            contexts: ['all'],
+        });
+    }
+
     startup.afterinstall();
-    });
+});
--- internal/runtime/startup.js
+++ internal/runtime/startup.js
@@ -1,13 +1,3 @@
-function afterinstall () {
-    const kAWCActions = {
-        'wmanextpage': '[AWC WMan] Open in an external window',
-    };
-    for (let key of Object.keys(kAWCActions)) {
-        chrome.contextMenus.create({
-            id: key,
-            title: kAWCActions[key],
-            type: 'normal',
-            contexts: ['all'],
-        });
-    }
+function afterinstall() {
+    
 }
