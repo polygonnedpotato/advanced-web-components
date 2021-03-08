a7b6d607075e8a94bb85779a62ddab8bfbe47a54
PolygonnedPotato <cuteiethecorgi@gmail.com>
2021-03-08 08:53:45 -0800

    new stuff

--- internal/windowmanager/wmanmain.js
+++ internal/windowmanager/wmanmain.js
@@ -1,7 +1,7 @@
 function newWindow(url, inorout) {
     if (inorout == "in") {
-      //notavaliableyet
+        //notavaliableyet
     } else if (inorout == "out") {
-      open(url, 'External Window', `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=600,height=200,left=-1000,top=-1000`);
+        open(url, 'External Window', `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=600,height=200,left=-1000,top=-1000`);
     }
-  }
+}
