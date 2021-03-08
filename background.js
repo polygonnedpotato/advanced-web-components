fb44d0d60e0d9cc18e4d79807c5f49c2178b99d1
PolygonnedPotato <cuteiethecorgi@gmail.com>
2021-03-08 11:09:19 -0800

    i think this will work

--- background.js
+++ background.js
@@ -1,13 +1,5 @@
-chrome.runtime.onInstalled.addListener(function () {
-    const kAWCActions = {
-        'wmanextpage': '[AWC WMan] Open in an external window',
-    };
-    for (let key of Object.keys(kAWCActions)) {
-        chrome.contextMenus.create({
-            id: key,
-            title: kAWCActions[key],
-            type: 'normal',
-            contexts: ['all'],
-        });
-    }
-});
+import * as startup from '/internal/runtime/startup.js';
+
+chrome.runtime.onInstalled(function() {
+    startup.afterinstall();
+    });
