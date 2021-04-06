b7419b21d6cbee6cda2629baaf32ab2d024b1bf7
elburg the polygonned potato <cuteiethecorgi@gmail.com>
2021-04-06 14:27:23 -0700

    transitioning into new icon theme formatting. startup script will run in a different way now.

--- internal/runtime/startup.js
+++ internal/runtime/startup.js
@@ -1,5 +1,7 @@
 function afterinstall(platform,browserversion,osplatform,osversion) {
-    localStorage.setItem("BrowserName", platform);
+    localStorage.setItem("BrowserName", platform)
     localStorage.setItem("BrowserVersion", browserversion)
-    localStorage.setItem()
+    localStorage.setItem("OSPlatformName", osplatform)
+    localStorage.setItem("OSVersion", osversion)
+    sessionStorage.setItem("startup.js-parsedjsonfile-manifest.json", JSON.parse())
 }
